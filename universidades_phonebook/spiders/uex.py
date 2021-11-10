a458677f32da3674039d270878aa81dc50c3f627
Stefan <stefangralo@gmail.com>
2021-11-10 22:54:18 +0100

    UEX

---
+++ universidades_phonebook/spiders/uex.py
@@ -0,0 +1,18 @@
+import scrapy
+
+
+class UexSpider(scrapy.Spider):
+    name = 'uex'
+    allowed_domains = ['unex.es']
+    start_urls = ['https://www.unex.es/conoce-la-uex/departamentos/listado_personal?idDpto=Y006&personal=1']
+
+
+    def parse(self, response):
+        baseurl = 'https://www.unex.es/conoce-la-uex/departamentos'
+
+        for contact in response.css('a[href*=ficha_personal]::attr(href)').getall(): 
+            url = baseurl + contact
+            yield scrapy.Request(response.urljoin(contact), self.parse_contact)
+
+    def parse_contact(self,response):
+        print(response.css())
